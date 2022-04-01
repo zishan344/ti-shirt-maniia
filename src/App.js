b4a40abd77ef047d2e914e52e34920814eb9d444
zishan344 <zishanahmed344@gmail.com>
2022-04-01 10:45:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import Home from './components/Home/Home';
-import Header from './components/Header/Header';
-import { Route, Routes } from 'react-router-dom';
-import OrderReview from './components/OrderReview/OrderReview';
-import Grandpa from './components/Grandpa/Grandpa';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./component/Header/Header";
+import Home from "./component/Home/Home";
+import OrderReview from "./component/orderReview/OrderReview";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        <Route path='orderreview' element={<OrderReview></OrderReview>}></Route>
-        <Route path='/grandpa' element={<Grandpa></Grandpa>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="home" element={<Home></Home>}></Route>
+        <Route
+          path="/orderreview"
+          element={<OrderReview></OrderReview>}
+        ></Route>
       </Routes>
     </div>
   );
